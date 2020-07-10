15d9a35297eb7ad18003951e0c4730801044a4e6
its-a-wrap <patrick_tweed@hotmail.co.uk>
2020-07-10 17:30:38 +0100

    now can identify multiple sets

--- src/index.js
+++ src/index.js
@@ -9,15 +9,7 @@ export class HarryPotterBasketCalculator {
             3: 0.9,
             4: 0.8,
             5: 0.75,
-        }
-
-        this.bookCount = {
-            1: 0,
-            2: 0,
-            3: 0,
-            4: 0,
-            5: 0,
-        }
+        };
 
         this.calculatePrice();
     }
@@ -28,41 +20,35 @@ export class HarryPotterBasketCalculator {
             return;
         };
 
-        let uniqueCount = 1;
-        uniqueCount = this.calculateUniqueCount(uniqueCount);
+        const sets = this.calculateSets();
 
-        if (this.allBooksAreUnique(uniqueCount)) {
-            this.price = this.books.length * 8 * this.discountsBySetSize[uniqueCount];
-        } else {
-            const discountedSubtotal = (uniqueCount) * 8 * this.discountsBySetSize[uniqueCount];
-            const undiscountedSubtotal = (this.books.length - (uniqueCount)) * 8;
-
-            this.price = discountedSubtotal + undiscountedSubtotal;
-        }
+        sets.forEach(set => {
+            this.price += set.length * 8 * this.discountsBySetSize[set.length];
+        })
     }
 
-    allBooksAreUnique(uniqueCount) {
-        return uniqueCount === 1 || uniqueCount === this.books.length;
-    }
-
-    calculateUniqueCount(uniqueCount) {
+    calculateSets() {
         this.books.sort();
-        for (let i = 0; i < this.books.length; i++) {
-            if (i != 0) {
-                if (this.books[i] != this.books[i - 1]) {
-                    uniqueCount++;
+        let sets = [[]];
+        this.books.forEach(book => {
+            let addedToSet = false;
+            for (let i = 0; i < sets.length; i++) {
+                if (!sets[i].includes(book)) {
+                    sets[i].push(book);
+                    addedToSet = true;
+                    break;
                 }
             }
-        }
-        return uniqueCount;
+            if (!addedToSet) {
+                sets.push([book]);
+            }
+        });
+
+        return sets;
     }
 
     getPrice() {
+        this.calculatePrice();
         return this.price;
-    }
-
-    addBook(book) {
-        this.books.push(book);
-        this.calculatePrice()
     }
 }
--- src/index.spec.js
+++ src/index.spec.js
@@ -3,13 +3,14 @@ import chai from 'chai';
 
 const { expect, should, assert } = chai;
 
-describe('HarryPotterBasketCalculator', () => {
+describe('GIVEN HarryPotterBasketCalculator', () => {
     describe('AND the customer is yet to add any books to their basket', () => {
         const harryPotterBasketCalculator = new HarryPotterBasketCalculator();
         it('SHOULD return the price as 0', () => {
             expect(harryPotterBasketCalculator.getPrice()).to.be.eql(0);
         });
     });
+
     describe('AND the customer adds 1 unique book to their basket', () => {
         const harryPotterBasketCalculator = new HarryPotterBasketCalculator([0]);
         it(`SHOULD not apply discount and the price should be '8 EUR'`, () => {
@@ -51,4 +52,30 @@ describe('HarryPotterBasketCalculator', () => {
             expect(harryPotterBasketCalculator.getPrice()).to.be.eql(38);
         });
     });
+
+    describe('AND the customer adds 5 unique books to their basket and 2 additional unique books', () => {
+        const harryPotterBasketCalculator = new HarryPotterBasketCalculator([0, 1, 2, 3, 4, 0, 1]);
+        it('SHOULD apply 25% discount to five books and 5% discount to two books', () => {
+            expect(harryPotterBasketCalculator.getPrice()).to.be.eql(45.2);
+        });
+    });
+
+    describe('AND the customer adds two sets of 4 unique books to their basket', () => {
+        const harryPotterBasketCalculator = new HarryPotterBasketCalculator([0, 1, 2, 3, 4, 0, 1, 2]);
+        it('SHOULD apply 20% discount all 8 books', () => {
+            expect(harryPotterBasketCalculator.getPrice()).to.be.eql(51.6);
+        });
+    });
 });
+
+describe('GIVEN calculateSets', () => {
+    describe('AND the input is 0, 0, 1, 2, 3, 4, 4', () => {
+        const harryPotterBasketCalculator = new HarryPotterBasketCalculator([0, 0, 1, 2, 3, 4, 4]);
+        it(`SHOULD group the books into two sets of '0, 1, 2, 3, 4' and '0, 4`, () => {
+            const sets = harryPotterBasketCalculator.calculateSets();
+            console.log(sets);
+
+            expect(sets).to.eql([[0, 1, 2, 3, 4], [0, 4]])
+        })
+    })
+})
